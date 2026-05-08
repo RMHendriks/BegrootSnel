cb1948f1b33b31bd4effc63f68f8159ad0746854
RMHendriks <rm.hedriks@gmail.com>
2026-05-08 21:21:34 +0200

    feat: add dashboard detail view, header sync button, and global styles
    - Dashboard detail view modal for budget editing
    - Header sync button with mobile hamburger menu
    - Global style refinements

--- BegrootSnelFrontEnd/src/app/components/header/header.ts
+++ BegrootSnelFrontEnd/src/app/components/header/header.ts
@@ -1,24 +1,26 @@
 import { Component } from '@angular/core';
 import { TransactionService } from '../../services/transaction-service';
-import { Router, RouterLink } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
 export class Header {
   isSyncing = false;
+  isMobileMenuOpen = false;
 
-  constructor(private transactionService: TransactionService, private router: Router) { }
+  constructor(
+    private transactionService: TransactionService,
+    private router: Router,
+  ) {}
 
   syncBankData() {
     this.isSyncing = true;
-
-    // Simuleer een API call naar je Quarkus backend
     this.transactionService.intializeTransactions().subscribe({
-      next: (response) => {
+      next: () => {
         this.router.navigateByUrl('/transactions', { skipLocationChange: true }).then(() => {
           this.router.navigate([this.router.url]);
         });
@@ -27,7 +29,15 @@ export class Header {
       error: (error) => {
         console.error('Fout bij synchroniseren van bankgegevens:', error);
         this.isSyncing = false;
-      }
+      },
     });
+  }
+
+  toggleMobileMenu(): void {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
+
+  closeMobileMenu(): void {
+    this.isMobileMenuOpen = false;
   }
 }
